ec6fe91bb870104f15860fad3cd6119714b8e2bb
saiyam <saiyam.mishra26@gmail.com>
2022-11-04 23:37:32 +0530

    Updates

---
+++ precache-manifest.34247a91ba8eb3b77b2177904b3805fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ba3a388773b429870b3",
+    "url": "/cwh-p1/static/css/main.451bb396.chunk.css"
+  },
+  {
+    "revision": "0ba3a388773b429870b3",
+    "url": "/cwh-p1/static/js/main.0ba3a388.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/cwh-p1/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/cwh-p1/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "bc0f29c2b772e1ee10b9",
+    "url": "/cwh-p1/static/js/runtime~main.bc0f29c2.js"
+  },
+  {
+    "revision": "f170a58c7800a8b1ce38c7e2e6d014d1",
+    "url": "/cwh-p1/index.html"
+  }
+];
